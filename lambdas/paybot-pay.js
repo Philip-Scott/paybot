428d3a579b1269f9d74e87e54599f8be5e04398c
Felipe Escoto <felescoto95@hotmail.com>
2018-03-01 09:46:10 -0600

    Add mock lambda

---
+++ lambdas/paybot-pay.js
@@ -0,0 +1,22 @@
+exports.handler = (event, context, callback) => {
+    const payer = event.payer;
+    const receiver = event.receiverID;
+    const receiverName = event.receiverName;
+    const tag = event.tag;
+    const teamID = event.teamID;
+    const amount = event.amount;
+
+    if (payer == undefined || receiver == undefined || receiverName == undefined || teamID == undefined || amount == undefined || payer == receiver || amount <= 0) {
+        callback(null, {error: "Server validation failed"});
+    }
+
+    let message = {
+        message: "Sent $" + amount + " to " + receiverName
+    };
+
+    if (tag != undefined) {
+        message.message += " for " + tag
+    }
+
+    callback(null, message);
+};
