acf2d1bce90191a55d6b2f320a11bb6a61e501c1
Felipe Escoto <felescoto95@hotmail.com>
2018-03-22 16:29:37 -0600

    Payment commands with Lambda connection (#1)
    * Matching
    
    * Messages
    
    * Regex fixes
    
    * Scape that dot
    
    * Limit amout and allow $
    
    * Add help + balance regex changes
    
    * Talking to the API
    
    * Add mock lambda
    
    * Linting fixes
    
    * More linting fixes
    
    * Added spaces to inner JSON object
    
    * working query/
    
    * Working setting in DB
    
    * Working multiple tags

--- bot/paybot.coffee
+++ bot/paybot.coffee
@@ -18,18 +18,129 @@
 # Author:
 #   philip-scott
 
+request = require('request')
+
+PAY_HELP = [
+  '- `pay X to @person` - Adds a payment of X to person',
+  '- `pay <x> to <person> for <EVENT>` - Adds a payment for an EVENT'
+]
+
 module.exports = (robot) ->
-  robot.respond /pay ([^ ]*) to ([^ ]*)$/i, (msg) ->
-    amount = parseFloat(msg.match[1])
-    msg.reply "DEMO: payment register for " + amount + " to " + msg.match[2]
-  robot.respond /pay ([^ ]*) to ([^ ]*) for ([^ ]*)$/i, (msg) ->
-    amount = parseFloat(msg.match[1])
-    msg.reply "DEMO: payment register for " + amount + " to " + msg.match[2] + " for " + msg.match[3]
-  robot.respond /balance$/i, (msg) ->
+  robot.respond /pay \$?([0-9]*\.?[0-9]?[0-9]?) to @([^ ]*)\s*$/i, (msg) ->
+    paymentValidator robot, msg
+
+  robot.respond /pay \$?([0-9]*\.?[0-9]?[0-9]?) to @([^ ]*) for ([\w|\s]*)\s*$/i, (msg) ->
+    paymentValidator robot, msg
+
+  robot.respond /balance\s*$/i, (msg) ->
     msg.reply "DEMO: Showing your team's balance: "
-  robot.respond /balance me$/i, (msg) ->
+
+  robot.respond /balance me\s*$/i, (msg) ->
     msg.reply "DEMO: Showing your balance: "
-  robot.respond /balance for ([^ ]*)$/i, (msg) ->
+
+  robot.respond /balance for ([\w|\s]*)\s*$/i, (msg) ->
     msg.reply "DEMO: Showing your team's balance for event " + msg.match[1]
-  robot.respond /balance me for ([^ ]*)$/i, (msg) ->
+
+  robot.respond /balance me for ([\w|\s]*)\s*$/i, (msg) ->
     msg.reply "DEMO: Showing your balance for event  " + msg.match[1]
+
+  robot.respond /pay(\s+help)?\s*$/, (msg) ->
+    sendMessage msg, PAY_HELP.join('\n')
+
+paymentValidator = (robot, msg) ->
+  amount = parseFloat(msg.match[1])
+  if amount <= 0 or isNaN amount
+    sendError msg, "Amount must be greater than 0"
+    return false
+
+  if amount > 2048
+    sendError msg, "You should not be sending this much money"
+    return false
+
+  payer = msg.message.user
+  receiver = robot.brain.userForName msg.match[2]
+
+  if receiver is null
+    sendError msg, "This user does not exist"
+    return false
+
+  if receiver.slack.is_bot or receiver.slack.is_restricted or receiver.slack.is_app_user
+    sendError msg, "This user cannot receive a payment"
+    return false
+
+  if (payer.is_bot or payer.is_restricted or payer.is_app_user)
+    sendError msg, "You're not allowed to send a payment"
+    return false
+
+  if (payer.id is receiver.slack.id)
+    sendError msg, "You're not allowed to send a payment to yourself"
+    return false
+
+  message = msg.match[3]
+
+  payload = {
+    'payer': payer.id,
+    'receiverID': receiver.slack.id,
+    'receiverName': receiver.slack.real_name,
+    'tag': message,
+    'teamID': receiver.slack.team_id,
+    'amount': amount
+  }
+
+  sendPayment msg, payload
+
+
+sendError = (msg, message) ->
+  msg.send({
+    attachments: [{
+        title: 'Error: ' + message,
+        fallback: 'Error: ' + message,
+        color: "#ed5353"
+    }],
+    username: process.env.HUBOT_SLACK_BOTNAME,
+    as_user: true,
+  })
+
+
+sendConfirmation = (msg, message) ->
+  msg.send({
+    attachments: [{
+        title: 'Success! ' + message,
+        fallback: 'Success! ' + message,
+        color: "#9bdb4d"
+    }],
+    username: process.env.HUBOT_SLACK_BOTNAME,
+    as_user: true,
+  })
+
+
+sendMessage = (msg, message) ->
+  msg.send({
+    attachments: [{
+        title: 'Your Help Is Here!',
+        fallback: message,
+        color: "#64BAFF",
+        text: message,
+        mrkdwn: true,
+        footer: "Made with :heart: for Valiendo Verga"
+    }],
+    username: process.env.HUBOT_SLACK_BOTNAME,
+    as_user: true,
+  })
+
+
+sendPayment = (msg, object) ->
+  request({
+    url: process.env.PAYBOT_URL + 'pay',
+    method: "POST",
+    json: true,
+    headers: {
+      'x-api-key': process.env.PAYBOT_API_KEY
+    },
+    body: object
+  }, (error, response, body) ->
+      if response.body.error isnt undefined
+        sendError msg, response.body.error
+      else
+        sendConfirmation msg, response.body.message
+  )
---
+++ lambdas/paybot-pay.js
@@ -0,0 +1,156 @@
+const aws = require('aws-sdk');
+const https = require('https');
+const dynamo = new aws.DynamoDB({
+    apiVersion: '2012-10-08',
+    region: "us-west-2",
+    httpOptions: {
+        agent: new https.Agent({
+            rejectUnauthorized: true,
+            keepAlive: true
+        })
+    }
+});
+
+const DEFAULT_TAG = "~value";
+
+exports.handler = (event, context, callback) => {
+    const payer = event.payer;
+    const receiver = event.receiverID;
+    const receiverName = event.receiverName;
+    let tag = event.tag;
+    const teamID = event.teamID;
+    const amount = event.amount;
+
+    if (!payer || !receiver || !receiverName || !teamID || !amount || payer == receiver || amount <= 0) {
+        callback(null, { error: "Server validation failed" });
+    }
+
+    let message = {
+        message: "Sent $" + amount + " to " + receiverName
+    };
+
+    if (tag != undefined) {
+        message.message += " for " + tag;
+    } else {
+        tag = '~value';
+    }
+
+    let payerItem = {
+        'team': { S: teamID },
+        'user': { S: payer },
+    };
+
+    let receiverItem = {
+        'team': { S: teamID },
+        'user': { S: receiver },
+    };
+
+    var receiverParams = {
+        KeyConditionExpression: '#t = :team and #u = :user',
+        TableName: 'paybot',
+        ExpressionAttributeNames: {
+            "#u": "user",
+            "#t": "team"
+        },
+        ExpressionAttributeValues: {
+            ':team': teamID,
+            ':user': receiver
+        },
+    };
+
+    var payerParams = {
+        KeyConditionExpression: '#t = :team and #u = :user',
+        TableName: 'paybot',
+        ExpressionAttributeNames: {
+            "#u": "user",
+            "#t": "team"
+        },
+        ExpressionAttributeValues: {
+            ':team': teamID,
+            ':user': payer
+        },
+    };
+
+    var docClient = new aws.DynamoDB.DocumentClient({ service: dynamo });
+
+    var queries = [];
+    queries.push (docClient.query(receiverParams).promise());
+    queries.push (docClient.query(payerParams).promise());
+
+    Promise.all(queries).then((results) => {
+        var receiverData = results[0].Items[0];
+        var payerData = results[1].Items[0];
+
+        var receiverValue = (receiverData && receiverData[tag]) ? Number(receiverData[tag]) : 0;
+        var payerValue = (payerData && payerData[tag]) ? Number(payerData[tag]) : 0;
+
+        receiverValue += amount;
+        payerValue -= amount;
+
+        receiverItem[tag] = { N: String(receiverValue) };
+        payerItem[tag] = { N: String(payerValue) };
+
+        receiverParams = {
+            TableName: 'paybot',
+            Key:{
+                "team": teamID,
+                "user": receiver
+            },
+            UpdateExpression: `set #k = :a`,
+            ExpressionAttributeNames:{
+                "#k": tag
+            },
+            ExpressionAttributeValues:{
+                ":a": receiverValue,
+            },
+            ReturnValues:"UPDATED_NEW"
+        };
+
+        payerParams = {
+            TableName: 'paybot',
+            Key:{
+                "team": teamID,
+                "user": payer
+            },
+            UpdateExpression: `set #k = :a`,
+            ExpressionAttributeNames:{
+                "#k": tag
+            },
+            ExpressionAttributeValues: {
+                ":a": payerValue,
+            },
+            ReturnValues:"UPDATED_NEW"
+        };
+
+        if(tag != DEFAULT_TAG) {
+            receiverValue = (receiverData && receiverData[DEFAULT_TAG]) ? Number(receiverData[DEFAULT_TAG]) : 0;
+            payerValue = (payerData && payerData[DEFAULT_TAG]) ? Number(payerData[DEFAULT_TAG]) : 0;
+
+            receiverValue += amount;
+            payerValue -= amount;
+
+            receiverItem[DEFAULT_TAG] = { N: String(receiverValue) };
+            payerItem[DEFAULT_TAG] = { N: String(payerValue) };
+
+            payerParams.UpdateExpression = 'set #k = :a, #b = :b';
+            receiverParams.UpdateExpression = 'set #k = :a, #b = :b';
+            payerParams.ExpressionAttributeNames['#b'] = DEFAULT_TAG;
+            receiverParams.ExpressionAttributeNames['#b'] = DEFAULT_TAG;
+            payerParams.ExpressionAttributeValues[':b'] = payerValue;
+            receiverParams.ExpressionAttributeValues[':b'] = receiverValue;
+        }
+
+        queries = [];
+
+        queries.push(docClient.update(receiverParams).promise());
+        queries.push(docClient.update(payerParams).promise());
+
+        Promise.all(queries).then(() => {
+            callback(null, message);
+        }).catch((err) => {
+            callback(null, { error: JSON.stringify(err) });
+        });
+    }).catch((err) => {
+        callback(null, { error: JSON.stringify(err) });
+    });
+};
